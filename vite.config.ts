83c37f070d1d1c0dec0ae7b25ff827adb4fb6683
henriquecapi <henriquecapi@hotmail.com>
2025-10-25 13:45:26 -0300

    Configure GitHub Actions for automatic deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/conversor-cvs_json/',
       server: {
         port: 3000,
         host: '0.0.0.0',
